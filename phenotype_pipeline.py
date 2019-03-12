3b4c425c69dd2d58c9da02f61f2692a3ccfc0c9a
valerio Arnaboldi <valerio.arnaboldi@gmail.som>
2019-03-12 12:46:30 -0700

    new code to support all_sentences

--- phenotype_pipeline.py
+++ phenotype_pipeline.py
@@ -20,9 +20,14 @@ def get_document_sentences(api_request_text):
     req = request.Request(api_endpoint, data, headers={'Content-type': 'application/json',
                                                        'Accept': 'application/json'})
     res = request.urlopen(req)
-    sentences = defaultdict(str)
+    sentences = defaultdict(list)
+    doc_key = "matched_sentences" if "include_match_sentences" in api_request_text else "all_sentences"
     for doc in json.loads(res.read().decode('utf-8')):
-        sentences[doc["identifier"].split("/")[1][7:].split(".")[0]] = doc["matched_sentences"]
+        sentences[doc["identifier"].split("/")[1][7:].split(".")[0]] = [re.sub(
+            '\\t', ' ', re.sub('\\s+', ' ', sent.replace('\n', '')).strip()) for sent in doc[doc_key] if sent and
+                                                                                                         sent != ""]
+        sentences[doc["identifier"].split("/")[1][7:].split(".")[0]] = [
+            sent for sent in sentences[doc["identifier"].split("/")[1][7:].split(".")[0]] if sent]
     return sentences
 
 
@@ -35,9 +40,9 @@ def main():
         ssl._create_default_https_context = ssl._create_unverified_context
     sentences = get_document_sentences(args.api_request_text)
     sent_rows = zip_longest(*[pap_sentences for pap_sentences in sentences.values()])
-    print(*[paper_id for paper_id in sentences.keys()])
+    print(*[paper_id for paper_id in sentences.keys()], sep="\t")
     for sent_row in sent_rows:
-        sent_row = [re.sub('\\t', ' ', re.sub('\\s+', ' ', sent.replace('\n', '')).strip()) if sent else "" for sent in
+        sent_row = [sent if sent else "" for sent in
                     sent_row]
         print(*sent_row, sep="\t")
 
